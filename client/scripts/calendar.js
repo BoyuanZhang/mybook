1b8fe4d3c2ab85d6060cead689254472f924ed22
RangerMauve <RangerMauve@hotmail.com>
2014-02-06 22:05:31 -0500

    Added rv-on-enter binding

--- client/scripts/calendar.js
+++ client/scripts/calendar.js
@@ -358,6 +358,15 @@ function relocateTask(e){
 rivets.binders.class = function(el, value){
 	el.classList.add(value);
 }
+rivets.binders["on-enter"] = function(el,value){
+	el.addEventListener("keypress",function (e) {
+		var code = e.keyCode || e.which;
+		if (code == 13 && !e.shiftKey) {
+			if("call" in value)value.call(el,el,e);
+			return false;
+		}
+	});
+}
 var binding = rivets.bind(document.body, {
 	calendar: cal,
 	daysInWeek: calendarDefaults.days,
